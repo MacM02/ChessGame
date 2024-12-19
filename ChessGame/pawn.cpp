fcf37324fb024753d9d8173af5b5fceb2b38373b
MACK MUIR-JEFFRYES <u1360490@umail.utah.edu>
2024-12-19 12:15:11 -0700

    Added diagonal isValidMove() check in pawn class.

--- ChessGame/pawn.cpp
+++ ChessGame/pawn.cpp
@@ -25,28 +25,34 @@ class Pawn : public Piece {
 
 		if (isValidSquare(destSquare)) {
 			xTravel = destSquare.first - currSquare.first;
-			yTravel = destSquare.second - destSquare.second;
+			yTravel = destSquare.second - currSquare.second;
 
-			// if the pawn is white
+			// if the pawn is white and on its starting square
 			if (this->color == "white" && this->currSquare.first == 1) {
 				return (xTravel == 1) && (yTravel == 0)
-					|| (xTravel == 2) && (yTravel == 0);
-			}
-			else if (this->color == "white") {
-				return (xTravel == 1) && (yTravel == 0);
+					|| (xTravel == 2) && (yTravel == 0)
+					|| ((xTravel == yTravel) && ((xTravel == 1) && (abs(yTravel) == 1)));
 			}
 
-			// if the pawn is black
+			// if pawn is white and has moved this game
+			else if (this->color == "white") {
+				return (xTravel == 1) && (yTravel == 0) 
+					|| ((xTravel == yTravel) && ((xTravel == 1) && (abs(yTravel) == 1)));
+			}
+
+			// if the pawn is black and on its starting square
 			if (this->color == "black" && this->currSquare.first == 6) {
 				return (xTravel == -1) && (yTravel == 0)
-					|| (xTravel == -2) && (yTravel == 0);
+					|| (xTravel == -2) && (yTravel == 0)
+					|| ((xTravel == yTravel) && ((xTravel == -1) && (abs(yTravel) == 1)));
 			}
+
+			// if pawn is black and has moved this game
 			else if (this->color == "black") {
-				return (xTravel == -1) && (yTravel == 0);
+				return (xTravel == -1) && (yTravel == 0) 
+					|| ((xTravel == yTravel) && ((xTravel == -1) && (abs(yTravel) == 1)));
 			}
 		}
 		return false;
 	};
-
-	bool 
 };
